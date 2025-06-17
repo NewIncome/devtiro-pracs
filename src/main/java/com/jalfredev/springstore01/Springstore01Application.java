e0828caefa448f3883a27481e8f99e232712c793
Alfredo C <alfredo_cardenas@live.com>
2025-06-16 21:23:19 -0600

    Add all main files for a clean Spring projectCreated with start.spring.io

---
+++ src/main/java/com/jalfredev/springstore01/Springstore01Application.java
@@ -0,0 +1,13 @@
+package com.jalfredev.springstore01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springstore01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springstore01Application.class, args);
+	}
+
+}
