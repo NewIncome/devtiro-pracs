b7b463adfbd6fe089eb19471f17b1a5ae97a8abe
Alfredo C <alfredo_cardenas@live.com>
2025-08-18 22:04:24 -0600

    Add core for first Spring DB practice project

---
+++ database01/src/main/java/com/devtiro/database01/Database01Application.java
@@ -0,0 +1,13 @@
+package com.devtiro.database01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Database01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Database01Application.class, args);
+	}
+
+}
---
+++ database01/src/test/java/com/devtiro/database01/Database01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtiro.database01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Database01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
