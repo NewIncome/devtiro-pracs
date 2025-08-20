3e2d46c3477c97bde19bbb498abec2f01fc06abc
Alfredo C <alfredo_cardenas@live.com>
2025-08-19 18:21:19 -0600

    Add configuration and code to start a local DB with H2(Hikari) - This type of DB is typically just for test or local info

--- database01/src/main/java/com/devtiro/database01/Database01Application.java
+++ database01/src/main/java/com/devtiro/database01/Database01Application.java
@@ -3,11 +3,29 @@ package com.devtiro.database01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
 @SpringBootApplication
-public class Database01Application {
+@Log
+public class Database01Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Database01Application.class, args);
-	}
+  //ConstructorInjection of DataSource
+  private final DataSource dataSource;
 
+  public Database01Application(final DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  //Main method
+	public static void main(String[] args) { SpringApplication.run(Database01Application.class, args); }
+
+  @Override
+  public void run(final String... args) {
+    log.info("Datasource: " + dataSource.toString());
+    final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+    restTemplate.execute("select 1");
+  }
 }
