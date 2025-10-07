99fffd0d57a4b6cf540c286adf8755604ba1df9c
Alfredo C <alfredo_cardenas@live.com>
2025-10-07 12:13:05 -0600

    Refactor: add more authorCreation methods to populate the DB with more authors - and fix the name on the createAuthor methods in other tests

--- database01/src/test/java/com/devtiro/database01/TestDataUtil.java
+++ database01/src/test/java/com/devtiro/database01/TestDataUtil.java
@@ -8,11 +8,25 @@ public final class TestDataUtil {   //final, usual patter for utility classes
   private TestDataUtil(){     //not going to expose the default constructor, we don't want the class constructed at all""
   }
 
-  public static Author createTestAuthor() {
+  public static Author createTestAuthorA() {
     return Author.builder()
             .id(1L)
             .name("Abigail Rose")
             .age(80)
+            .build();
+  }
+  public static Author createTestAuthorB() {
+    return Author.builder()
+            .id(2L)
+            .name("Thomas Cronin")
+            .age(44)
+            .build();
+  }
+  public static Author createTestAuthorC() {
+    return Author.builder()
+            .id(3L)
+            .name("Jesse A Casey")
+            .age(24)
             .build();
   }
 
