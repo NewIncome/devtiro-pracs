799003f776379b768581e4551cfa6c9b7fa011c9
Alfredo C <alfredo_cardenas@live.com>
2025-11-05 18:52:45 -0600

    feat: add files for the Book Web-layer (pseudoPresentation-layer)

---
+++ database/2_author_create/src/main/java/com/devtiro/database/controllers/BookController.java
@@ -0,0 +1,49 @@
+/**
+ * BookController End-Points
+ *    PUT     /books/{isbn}   //Create 1 book; response->HTTP:201 + bookJson
+ *    GET     /books/{isbn}   //Read 1 book; response-> HTTP:200 + bookJson
+ *    GET     /books          //ReadMany books; always response-> HTTP:200 + emptyList || bookListJson
+ *    PUT     /books/{isbn}   //Update; response-> HTTP:200 + bookJson
+ *    PATCH   /books/{isbn}   //PartialUpdate; response-> HTTP:200 + bookJson
+ *    DELETE  /books/{isbn}   //Delete; response-> HTTP:204 + noBody
+ */
+package com.devtiro.database.controllers;
+
+import com.devtiro.database.domain.dto.BookDto;
+import com.devtiro.database.domain.entities.BookEntity;
+import com.devtiro.database.mappers.Mapper;
+import com.devtiro.database.services.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+  private Mapper<BookEntity, BookDto> bookMapper;
+
+  private BookService bookService;
+
+  public BookController(Mapper<BookEntity, BookDto> bookMapper, BookService bookService) {
+    this.bookMapper = bookMapper;
+    this.bookService = bookService;
+  }
+
+  @PutMapping("/books/{isbn}")  //for a FULL-UPDATE or CREATE(w-CustomId)
+  public ResponseEntity<BookDto> createBook(@PathVariable("isbn") String isbn,
+                                            @RequestBody BookDto bookDto) { //variable to assign PathVariable to
+    /* to map our Dto to our Entity to use it in our Service for our PersistenceLayer
+       mapDtoToEntity > Service > PersistenceLayer */
+    BookEntity bookEntity = bookMapper.mapFrom(bookDto);
+    //to create a Book we need to interact with our service layer
+    BookEntity savedBookEntity = bookService.createBook(isbn, bookEntity);
+    //we need to return a BookDto from this
+    BookDto savedBookDto = bookMapper.mapTo(savedBookEntity);
+    return new ResponseEntity<>(savedBookDto, HttpStatus.CREATED);
+  }
+
+}
