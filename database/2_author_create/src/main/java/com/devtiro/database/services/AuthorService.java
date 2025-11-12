1bf4ecbc3d259ffa17691abed6b29b4fc267d21a
Alfredo C <alfredo_cardenas@live.com>
2025-11-11 19:56:33 -0600

    feat: create the findAll() method in the AuthorService Interface

--- database/2_author_create/src/main/java/com/devtiro/database/services/AuthorService.java
+++ database/2_author_create/src/main/java/com/devtiro/database/services/AuthorService.java
@@ -6,5 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AuthorService {
-    AuthorEntity save(AuthorEntity authorEntity);
+  AuthorEntity save(AuthorEntity authorEntity);
+
+  List<AuthorEntity> findAll();
 }
