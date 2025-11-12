f54f1df2e6fafcfda17829222ee8b29763fb09f7
Alfredo C <alfredo_cardenas@live.com>
2025-11-11 19:57:29 -0600

    feat: create the AuthorService findAll() method implementation

--- database/2_author_create/src/main/java/com/devtiro/database/services/impl/AuthorServiceImpl.java
+++ database/2_author_create/src/main/java/com/devtiro/database/services/impl/AuthorServiceImpl.java
@@ -13,14 +13,27 @@ import java.util.stream.StreamSupport;
 @Service
 public class AuthorServiceImpl implements AuthorService {
 
-    private AuthorRepository authorRepository;
+  private AuthorRepository authorRepository;
 
-    public AuthorServiceImpl(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }
+  public AuthorServiceImpl(AuthorRepository authorRepository) {
+      this.authorRepository = authorRepository;
+  }
 
-    @Override
-    public AuthorEntity save(AuthorEntity authorEntity) {
+  @Override
+  public AuthorEntity save(AuthorEntity authorEntity) {
         return authorRepository.save(authorEntity);
     }
+
+  @Override
+  public List<AuthorEntity> findAll() {
+    //authorRepository.findAll(); //it returns an iterable, to make it a list we do:
+    return StreamSupport    /*Low-level utility methods for creating and manipulating streams*/
+              .stream(      /*Creates a new sequential or parallel Stream from a Spliterator*/
+                  authorRepository      /*the class that extends the CrudRepository class querying-functionality*/
+                        .findAll()      /*returns an iterable; extended CrudRepository-class-method that Returns all instances of the type*/
+                        .spliterator(), /*Creates a Spliterator over the elements described by this Iterable*/
+                  false)    /*to define the stream as true=parallel, false=sequential*/
+              .collect(Collectors.toList());  //to collect/convert the resulting stream into a List
+  }
+
 }
