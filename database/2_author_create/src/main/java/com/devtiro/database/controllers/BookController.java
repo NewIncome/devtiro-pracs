7a74ab48fbe7372775e20f45c49fe28c614716b4
Alfredo C <alfredo_cardenas@live.com>
2025-11-13 17:50:57 -0600

    feat: add the get/listManyBooks end-point n method

--- database/2_author_create/src/main/java/com/devtiro/database/controllers/BookController.java
+++ database/2_author_create/src/main/java/com/devtiro/database/controllers/BookController.java
@@ -16,10 +16,10 @@ import com.devtiro.database.services.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class BookController {
@@ -44,6 +44,14 @@ public class BookController {
     //we need to return a BookDto from this
     BookDto savedBookDto = bookMapper.mapTo(savedBookEntity);
     return new ResponseEntity<>(savedBookDto, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/books")
+  public List<BookDto> listBooks() {
+    List<BookEntity> books = bookService.findAll(); //use pagination later
+    return books.stream()
+                  .map(bookMapper::mapTo)
+                  .collect(Collectors.toList());
   }
 
 }
