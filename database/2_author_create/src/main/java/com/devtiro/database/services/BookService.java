7e192781afdb486ff7c168f3b7480635de4d482b
Alfredo C <alfredo_cardenas@live.com>
2025-11-13 17:52:30 -0600

    feat: declare the findAll() method in the AuthorService int - and define the implementation in the AuthorServiceImpl

--- database/2_author_create/src/main/java/com/devtiro/database/services/BookService.java
+++ database/2_author_create/src/main/java/com/devtiro/database/services/BookService.java
@@ -2,8 +2,12 @@ package com.devtiro.database.services;
 
 import com.devtiro.database.domain.entities.BookEntity;
 
+import java.util.List;
+
 public interface BookService {
 
   BookEntity createBook(String isbn, BookEntity bookEntity);
+
+  List<BookEntity> findAll();
 
 }
