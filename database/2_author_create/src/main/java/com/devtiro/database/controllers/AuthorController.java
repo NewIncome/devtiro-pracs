26105175def886f603c2f8ed0f6d974f0c4804c8
Alfredo C <alfredo_cardenas@live.com>
2025-11-14 19:06:20 -0600

    feat: implement the EndPoint for '/authors/id' by getAuthor() method

--- database/2_author_create/src/main/java/com/devtiro/database/controllers/AuthorController.java
+++ database/2_author_create/src/main/java/com/devtiro/database/controllers/AuthorController.java
@@ -13,14 +13,13 @@ import com.devtiro.database.domain.dto.AuthorDto;
 import com.devtiro.database.domain.entities.AuthorEntity;
 import com.devtiro.database.mappers.Mapper;
 import com.devtiro.database.services.AuthorService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -51,7 +50,20 @@ public class AuthorController {
     List<AuthorEntity> authors = authorService.findAll();  //that's what we receive from querying the DB
     return authors.stream()
             .map(authorMapper::mapTo)
-            .collect(Collectors.toList());
+            .collect(Collectors.toList());  //must return an AuthorDto/Pojo
+  }
+
+  //To create the ReadOne end-point
+  @GetMapping(path = "/authors/{id}")
+  public ResponseEntity<AuthorDto> getAuthor(@PathVariable("id") Long id) {
+    /* Returns an Optional because it can find it or it won't
+    * so it'll return an Optional<Author> or an Optional<> empty */
+    Optional<AuthorEntity> foundAuthor = authorService.findOne(id);
+    return foundAuthor.map(authorEntity -> { //to convert the Optional to a ResponseEntity
+      //in case we found the author, convert AuthorEntity to AuthorDto
+      AuthorDto authorDto = authorMapper.mapTo(authorEntity);
+      return new ResponseEntity<>(authorDto, HttpStatus.OK);
+    }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 
 }
