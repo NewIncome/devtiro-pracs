de239abaebae5fe2eb7e289d248df3be68adf92b
Alfredo C <alfredo_cardenas@live.com>
2025-11-25 22:11:56 -0600

    feat: create the partialUpdate AuthorService implementation

--- database/2_author_create/src/main/java/com/devtiro/database/services/impl/AuthorServiceImpl.java
+++ database/2_author_create/src/main/java/com/devtiro/database/services/impl/AuthorServiceImpl.java
@@ -1,5 +1,6 @@
 package com.devtiro.database.services.impl;
 
+import com.devtiro.database.domain.dto.AuthorDto;
 import com.devtiro.database.domain.entities.AuthorEntity;
 import com.devtiro.database.repositories.AuthorRepository;
 import com.devtiro.database.services.AuthorService;
@@ -49,6 +50,20 @@ public class AuthorServiceImpl implements AuthorService {
   @Override
   public AuthorEntity update(AuthorEntity authorEntity) {
     return authorRepository.save(authorEntity);
+  }
+
+  @Override
+  public AuthorEntity partialUpdate(Long id, AuthorEntity authorEntity) {
+    authorEntity.setId(id);
+    /* In order to update the DB, 1st we need to retrieve the DB then update */
+    return authorRepository.findById(id).map(existingAuthor -> {
+      /* If the Optional has an Age and is notNull,
+         set that as we found it on the existing author as we found it in the DB */
+      //.ofNullable(), returns an emptyOptional on Null; .of() doesn't
+      Optional.ofNullable(authorEntity.getAge()).ifPresent(existingAuthor::setAge);
+      Optional.ofNullable(authorEntity.getName()).ifPresent(existingAuthor::setName);
+      return authorRepository.save(existingAuthor);
+    }).orElseThrow(() -> new RuntimeException("Author does not exist"));
   }
 
 }
