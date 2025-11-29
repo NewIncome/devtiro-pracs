8b972c414274b691bd627ae8d2c5d2c10e31b01f
Alfredo C <alfredo_cardenas@live.com>
2025-11-28 19:35:25 -0600

    feat: Add in ModelMapper a MappingStrategy for NestedEntities added CRUD func

--- database/2_author_create/src/main/java/com/devtiro/database/config/MapperConfig.java
+++ database/2_author_create/src/main/java/com/devtiro/database/config/MapperConfig.java
@@ -10,6 +10,20 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+
+      ModelMapper modelMapper = new ModelMapper();
+      /* Incantation to:
+       * "Match" Nested  Objects and allow us to convert from one to the other.
+       * Thus,
+       * When we send a nested-object within the DTO it will then be converted
+       * to a nested-Entity-Object,
+       * and that! is all JPA needs in order to "Cascade" and do those saves and
+       * do those updates
+       * To really harness the power of NestedObjects in Spring-Data-JPA */
+      modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+
+      return modelMapper;
+
     }
+
 }
