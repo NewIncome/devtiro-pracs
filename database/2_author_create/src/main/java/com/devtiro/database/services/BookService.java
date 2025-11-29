289085106e5bc532b71aabf56ce179e37fc2666e
Alfredo C <alfredo_cardenas@live.com>
2025-11-28 19:39:50 -0600

    feat: add the definition of the paginated findAll method for Books

--- database/2_author_create/src/main/java/com/devtiro/database/services/BookService.java
+++ database/2_author_create/src/main/java/com/devtiro/database/services/BookService.java
@@ -1,6 +1,8 @@
 package com.devtiro.database.services;
 
 import com.devtiro.database.domain.entities.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,9 @@ public interface BookService {
   BookEntity createUpdateBook(String isbn, BookEntity bookEntity);
 
   List<BookEntity> findAll();
+
+  //overload the findAll() method to have them returning a List and a Paginated'Page'
+  Page<BookEntity> findAll(Pageable pageable);
 
   Optional<BookEntity> findOne(String isbn);
 
