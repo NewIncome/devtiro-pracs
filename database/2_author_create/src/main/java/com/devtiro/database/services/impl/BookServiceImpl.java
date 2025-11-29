c983a641c2e6361960e34a439a188ce637412b5e
Alfredo C <alfredo_cardenas@live.com>
2025-11-28 19:41:22 -0600

    feat: add the implementation for the paginated findAll method for Books

--- database/2_author_create/src/main/java/com/devtiro/database/services/impl/BookServiceImpl.java
+++ database/2_author_create/src/main/java/com/devtiro/database/services/impl/BookServiceImpl.java
@@ -3,6 +3,8 @@ package com.devtiro.database.services.impl;
 import com.devtiro.database.domain.entities.BookEntity;
 import com.devtiro.database.repositories.BookRepository;
 import com.devtiro.database.services.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,6 +35,11 @@ public class BookServiceImpl implements BookService {
                     bookRepository.findAll().spliterator(),
                     false)
               .collect(Collectors.toList());
+  }
+
+  @Override
+  public Page<BookEntity> findAll(Pageable pageable) {
+    return bookRepository.findAll(pageable);
   }
 
   @Override
